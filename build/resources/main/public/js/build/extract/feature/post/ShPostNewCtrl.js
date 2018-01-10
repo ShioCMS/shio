23df952eb032f688dca6a95df5217745e7c4e339
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-01-10 16:35:33 -0200

    - New Post

--- build/resources/main/public/js/build/extract/feature/post/ShPostNewCtrl.js
+++ build/resources/main/public/js/build/extract/feature/post/ShPostNewCtrl.js
@@ -6,18 +6,20 @@ shioharaApp.controller('ShPostNewCtrl', [
 		"$state",
 		"$rootScope",
 		"shAPIServerService",
-		function($scope, $http, $window, $stateParams, $state, $rootScope, shAPIServerService) {
+		"shPostResource",
+		function($scope, $http, $window, $stateParams, $state, $rootScope,
+				shAPIServerService, shPostResource) {
 			$scope.postTypeId = $stateParams.postTypeId;
 			$scope.shPost = null;
 			$scope.$evalAsync($http.get(
-					shAPIServerService.get().concat("/api/post/type/" + $scope.postTypeId
-							+ "/post/model").then(function(response) {
-				$scope.shPost = response.data;
-			})));
+					shAPIServerService.get().concat(
+							"/post/type/" + $scope.postTypeId + "/post/model"))
+					.then(function(response) {
+						$scope.shPost = response.data;
+					}));
 			$scope.postEditForm = "template/post/form.html";
 			$scope.postSave = function() {
-				$scope.shPost.$update(function() {
-					$state.go('content');
-				});
+				delete $scope.shPost.id;
+				shPostResource.save($scope.shPost);
 			}
 		} ]);
