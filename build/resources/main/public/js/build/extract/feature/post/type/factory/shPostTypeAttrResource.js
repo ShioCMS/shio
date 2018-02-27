18c59e34ea36883a1fa65afc8e11f403e9a87bd1
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-02-27 17:12:20 -0300

    - Removed build folder

--- build/resources/main/public/js/build/extract/feature/post/type/factory/shPostTypeAttrResource.js
+++
@@ -1,9 +0,0 @@
-shioharaApp.factory('shPostTypeAttrResource', [ '$resource', 'shAPIServerService', function($resource, shAPIServerService) {
-	return $resource(shAPIServerService.get().concat('/post/type/attr/:id'), {
-		id : '@id'
-	}, {
-		update : {
-			method : 'PUT'
-		}
-	});
-} ]);
