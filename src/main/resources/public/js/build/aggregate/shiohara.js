8eaed7743540301bc7ec93bb14286ff6e5af50c8
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-03-04 10:04:28 -0300

    - StaticFile: Select File

--- src/main/resources/public/js/build/aggregate/shiohara.js
+++ src/main/resources/public/js/build/aggregate/shiohara.js
@@ -883,8 +883,26 @@ shioharaApp.controller('ShWidgetFileCtrl', [ '$scope', 'Upload', '$timeout','$ui
                 });
             }
 		} ]);
-shioharaApp.controller('ShWidgetFileSelectCtrl', [ '$scope', 'Upload', '$timeout',
-		function($scope, Upload, $timeout) {		
+shioharaApp.controller('ShWidgetFileSelectCtrl', [
+		'$scope',
+		'shAPIServerService',
+		'$http',
+		function($scope, shAPIServerService, $http) {
+			$scope.channelId = "83041422-e6d9-444f-bb82-2a6dbb9ba3c2";
+			$scope.shSite = null;
+			$scope.shChannels = null;
+			$scope.shPosts = null;
+			$scope.breadcrumb = null;
+
+			$scope.$evalAsync($http.get(
+					shAPIServerService.get().concat(
+							"/channel/" + $scope.channelId + "/list")).then(
+					function(response) {
+						$scope.shChannels = response.data.shChannels;
+						$scope.shPosts = response.data.shPosts;
+						$scope.breadcrumb = response.data.breadcrumb;
+						$scope.shSite = response.data.shSite;
+					}));
 		} ]);
 shioharaApp.factory('shWidgetResource', [ '$resource', 'shAPIServerService', function($resource, shAPIServerService) {
 	return $resource(shAPIServerService.get().concat('/widget/:id'), {
--- src/main/resources/public/js/build/extract/feature/widget/file/ShWidgetFileSelectCtrl.js
+++ src/main/resources/public/js/build/extract/feature/widget/file/ShWidgetFileSelectCtrl.js
@@ -1,3 +1,21 @@
-shioharaApp.controller('ShWidgetFileSelectCtrl', [ '$scope', 'Upload', '$timeout',
-		function($scope, Upload, $timeout) {		
+shioharaApp.controller('ShWidgetFileSelectCtrl', [
+		'$scope',
+		'shAPIServerService',
+		'$http',
+		function($scope, shAPIServerService, $http) {
+			$scope.channelId = "83041422-e6d9-444f-bb82-2a6dbb9ba3c2";
+			$scope.shSite = null;
+			$scope.shChannels = null;
+			$scope.shPosts = null;
+			$scope.breadcrumb = null;
+
+			$scope.$evalAsync($http.get(
+					shAPIServerService.get().concat(
+							"/channel/" + $scope.channelId + "/list")).then(
+					function(response) {
+						$scope.shChannels = response.data.shChannels;
+						$scope.shPosts = response.data.shPosts;
+						$scope.breadcrumb = response.data.breadcrumb;
+						$scope.shSite = response.data.shSite;
+					}));
 		} ]);
