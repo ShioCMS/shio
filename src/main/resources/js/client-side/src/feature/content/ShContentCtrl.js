29e7dbc26ddca84221c1a21f15c0ebe6165f4d1e
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-03-24 09:40:04 -0300

    - LastPost and ShPost fix

--- src/main/resources/js/client-side/src/feature/content/ShContentCtrl.js
+++ src/main/resources/js/client-side/src/feature/content/ShContentCtrl.js
@@ -1,6 +1,6 @@
 shioharaApp.controller('ShContentCtrl', [ "$rootScope", "$scope","$state",
-		'vigLocale', "$translate",
-		function($rootScope, $scope, $state, vigLocale, $translate) {
+		'vigLocale', "$translate", "shUserResource", "shPostTypeResource",
+		function($rootScope, $scope, $state, vigLocale, $translate, shUserResource, shPostTypeResource) {
 			$scope.channelId = null;
 			$scope.shUser = null;
 			$scope.shSite = null;
@@ -9,5 +9,15 @@ shioharaApp.controller('ShContentCtrl', [ "$rootScope", "$scope","$state",
 			$scope.shChannels = null;
 			$rootScope.$state = $state;
 			$scope.breadcrumb = null;
+			
+			$scope.shUser = shUserResource.get({
+				id : 1,
+				access_token : $scope.accessToken
+			}, function() {
+				$scope.shLastPostType = shPostTypeResource.get({
+					id : $scope.shUser.lastPostType
+				});
+				
+			});
 
 		} ]);
