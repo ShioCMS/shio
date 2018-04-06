44e23a44a94321c40bd32d2c5a169d8d5d72ac90
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-04-06 20:59:58 -0300

    - Spring Boot 2.0.1: Undertow and Hibernate Search

--- src/main/java/com/viglet/shiohara/ShApplication.java
+++ src/main/java/com/viglet/shiohara/ShApplication.java
@@ -1,8 +1,8 @@
 package com.viglet.shiohara;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
@@ -11,23 +11,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 
-import javax.sql.DataSource;
+import io.undertow.UndertowOptions;
 
 @SpringBootApplication
 @EnableJms
 @EnableCaching
-public class Application {
-	@Autowired
-	DataSource dataSource;
+public class ShApplication {
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ShApplication.class, args);
 	}
 
 	@Bean
 	public Module hibernate5Module() {
 		return new Hibernate5Module();
-	}	
+	}
+
+	@Bean
+	UndertowServletWebServerFactory embeddedServletContainerFactory() {
+		UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
+		factory.addBuilderCustomizers(builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true));
+		return factory;
+	}
+
 	@RequestMapping("/")
 	String index() {
 		return "index";
--- src/main/java/com/viglet/shiohara/api/search/ShSearchAPI.java
+++ src/main/java/com/viglet/shiohara/api/search/ShSearchAPI.java
@@ -15,7 +15,7 @@ import com.viglet.shiohara.api.post.ShPostWIthBreadcrumb;
 import com.viglet.shiohara.persistence.model.folder.ShFolder;
 import com.viglet.shiohara.persistence.model.post.ShPost;
 import com.viglet.shiohara.persistence.model.site.ShSite;
-import com.viglet.shiohara.search.HibernateSearchService;
+import com.viglet.shiohara.persistence.repository.post.ShPostRepository;
 import com.viglet.shiohara.utils.ShFolderUtils;
 
 import io.swagger.annotations.Api;
@@ -23,20 +23,20 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/api/v2/search")
-@Api(tags="Search", description="Search for Shiohara Objects")
+@Api(tags = "Search", description = "Search for Shiohara Objects")
 public class ShSearchAPI {
 
 	@Autowired
-	private HibernateSearchService searchservice;
+	private ShPostRepository shPostRepository;
 	@Autowired
 	private ShFolderUtils shFolderUtils;
 
-	@ApiOperation(value = "Search for Shiohara Objects")    
+	@ApiOperation(value = "Search for Shiohara Objects")
 	@RequestMapping(method = RequestMethod.GET)
-	@JsonView({  ShJsonView.ShJsonViewObject.class })
+	@JsonView({ ShJsonView.ShJsonViewObject.class })
 	public List<ShPostWIthBreadcrumb> shSearch(@RequestParam(value = "q") String q) throws Exception {
 		List<ShPostWIthBreadcrumb> searchResults = new ArrayList<ShPostWIthBreadcrumb>();
-		for (ShPost shPost : searchservice.fuzzySearch(q)) {
+		for (ShPost shPost : shPostRepository.fuzzySearch(q)) {
 			ArrayList<ShFolder> breadcrumb = shFolderUtils.breadcrumb(shPost.getShFolder());
 			ShSite shSite = breadcrumb.get(0).getShSite();
 			ShPostWIthBreadcrumb shPostWIthBreadcrumb = new ShPostWIthBreadcrumb();
