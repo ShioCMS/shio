9d882506efbed1a057aa98477ee5dca1c3656e63
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-04-11 23:19:54 -0300

    - Dev Profile

---
+++ src/main/java/com/viglet/shiohara/spring/security/ShSecurityConfigDevelopment.java
@@ -0,0 +1,47 @@
+package com.viglet.shiohara.spring.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+@Profile("development")
+public class ShSecurityConfigDevelopment extends WebSecurityConfigurerAdapter {
+	@Autowired
+	ShAccessDenied shAccessDenied;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.headers().cacheControl().disable();
+		http.csrf().disable();
+		http.cors().disable();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/thirdparty/**", "/js/**","/css/**", "/template/**","/img/**","/sites/**","/swagger-resources/**");
+	}
+
+	// create two users, admin and user
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER").and().withUser("admin")
+				.password("{noop}admin").roles("ADMIN");
+	}
+
+	private CsrfTokenRepository csrfTokenRepository() {
+		  HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		  repository.setHeaderName("X-XSRF-TOKEN");
+		  return repository;
+		}
+}
