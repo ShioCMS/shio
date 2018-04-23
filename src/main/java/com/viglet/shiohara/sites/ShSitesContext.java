482fdda534f4a082eb54c573e1914ff8b3cdc36a
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-04-23 00:51:09 -0300

    - Split Sites Context Components

--- src/main/java/com/viglet/shiohara/sites/ShSitesContext.java
+++ src/main/java/com/viglet/shiohara/sites/ShSitesContext.java
@@ -1,31 +1,16 @@
 package com.viglet.shiohara.sites;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
-import javax.annotation.Resource;
-import javax.script.Bindings;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.io.ResourceLoader;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,7 +22,6 @@ import com.viglet.shiohara.persistence.model.folder.ShFolder;
 import com.viglet.shiohara.persistence.model.post.ShPost;
 import com.viglet.shiohara.persistence.model.post.ShPostAttr;
 import com.viglet.shiohara.persistence.model.site.ShSite;
-import com.viglet.shiohara.persistence.repository.folder.ShFolderRepository;
 import com.viglet.shiohara.persistence.repository.post.ShPostRepository;
 import com.viglet.shiohara.persistence.repository.site.ShSiteRepository;
 import com.viglet.shiohara.post.type.ShSystemPostType;
@@ -53,18 +37,13 @@ public class ShSitesContext {
 	@Autowired
 	private ShSiteRepository shSiteRepository;
 	@Autowired
-	private ShFolderRepository shFolderRepository;
-	@Autowired
 	private ShFolderUtils shFolderUtils;
 	@Autowired
 	private ShPostUtils shPostUtils;
 	@Autowired
 	private ShSiteUtils shSiteUtils;
 	@Autowired
-	private ResourceLoader resourceloader;
-
-	@Resource
-	private ApplicationContext context;
+	private ShSitesContextComponent shSitesContextComponent;
 
 	@RequestMapping("/sites/**")
 	private void sitesFullGeneric(HttpServletRequest request, HttpServletResponse response)
@@ -133,205 +112,19 @@ public class ShSitesContext {
 		}
 	}
 
-	private StringBuilder shObjectJSFactory() throws IOException {
-		InputStreamReader isr = new InputStreamReader(
-				resourceloader.getResource("classpath:/js/server-side/shObject.js").getInputStream());
-
-		StringBuilder shObjectJS = new StringBuilder();
-		try (Reader reader = new BufferedReader(isr)) {
-			int c = 0;
-			while ((c = reader.read()) != -1) {
-				shObjectJS.append((char) c);
-			}
-		}
-		return shObjectJS;
-	}
-
-	private String folderPathFactory(ArrayList<String> contentPath) {
-		String folderPath = "/";
-		if (contentPath.size() >= 1) {
-			ArrayList<String> folderPathArray = contentPath;
-
-			// Remove PostName
-			folderPathArray.remove(folderPathArray.size() - 1);
-
-			for (String path : folderPathArray) {
-				folderPath = folderPath + path + "/";
-			}
-
-		}
-		return folderPath;
-	}
-
-	private ArrayList<String> contentPathFactory(int contextPathPosition, HttpServletRequest request) {
-		String url = (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
-		String[] contexts = url.split("/");
-		ArrayList<String> contentPath = new ArrayList<String>();
-
-		for (int i = contextPathPosition; i < contexts.length; i++) {
-			contentPath.add(contexts[i]);
-		}
-		return contentPath;
-	}
-
-	private ShPost shPostAlias(ShPost shPostItem) {
-		if (shPostItem.getShPostType().getName().equals(ShSystemPostType.ALIAS)) {
-			List<ShPostAttr> shPostAttrs = shPostItem.getShPostAttrs();
-			for (ShPostAttr shPostAttr : shPostAttrs) {
-				if (shPostAttr.getShPostTypeAttr().getName().equals(ShSystemPostTypeAttr.CONTENT)) {
-					shPostItem = shPostRepository.findById(UUID.fromString(shPostAttr.getStrValue())).get();
-				}
-			}
-		}
-		return shPostItem;
-	}
-
-	private String objectNameFactory(ArrayList<String> contentPath) {
-		String objectName = null;
-		int lastPosition = contentPath.size() - 1;
-		if (contentPath.size() >= 1) {
-			objectName = contentPath.get(lastPosition);
-		}
-		return objectName;
-	}
-
-	private ShPost shPostItemFactory(ShSite shSite, String folderPath, String objectName) {
-		ShPost shPostItem = null;
-
-		// If shPostItem is not null, so is a Post, otherwise is a Folder
-		if (objectName != null) {
-			ShFolder shParentFolder = shFolderUtils.folderFromPath(shSite, folderPath);
-			shPostItem = shPostRepository.findByShFolderAndFurl(shParentFolder, objectName);
-		}
-
-		if (shPostItem != null) {
-			shPostItem = this.shPostAlias(shPostItem);
-		} else {
-			String folderPathCurrent = folderPath;
-			if (objectName != null) {
-				folderPathCurrent = folderPathCurrent + objectName + "/";
-			}
-			ShFolder shFolderItem = shFolderUtils.folderFromPath(shSite, folderPathCurrent);
-			if (shFolderItem != null) {
-				// System.out.println("shFolderItem is not null");
-				ShPost shFolderIndex = shPostRepository.findByShFolderAndFurl(shFolderItem, "index");
-
-				if (shFolderIndex != null) {
-					shPostItem = shFolderIndex;
-				}
-			}
-		}
-		return shPostItem;
-	}
-
-	private JSONObject shThemeFactory(String themeName) {
-		ShPost shTheme = shPostRepository.findByTitle(themeName);
-
-		Map<String, ShPostAttr> shThemeMap = shPostUtils.postToMap(shTheme);
-
-		JSONObject shThemeAttrs = new JSONObject();
-		shThemeAttrs.put("javascript", shThemeMap.get(ShSystemPostTypeAttr.JAVASCRIPT).getStrValue());
-		shThemeAttrs.put("css", shThemeMap.get(ShSystemPostTypeAttr.CSS).getStrValue());
-
-		return shThemeAttrs;
-	}
-
-	private JSONArray shPostItemsFactory(ShFolder shFolderItem) {
-		JSONArray shPostItems = new JSONArray();
-		List<ShPost> shPosts = shPostRepository.findByShFolder(shFolderItem);
-
-		for (ShPost shPost : shPosts) {
-			if (!shPost.getShPostType().getName().equals(ShSystemPostType.FOLDER_INDEX)) {
-				JSONObject shPostItemAttrs = shPostUtils.toJSON(shPost);
-				shPostItems.put(shPostItemAttrs);
-			}
-		}
-
-		return shPostItems;
-	}
-
-	private JSONArray shChildFolderItemsFactory(ShFolder shFolderItem) {
-		JSONArray shChildFolderItems = new JSONArray();
-		List<ShFolder> shFolders = shFolderRepository.findByParentFolder(shFolderItem);
-
-		for (ShFolder shChildFolder : shFolders) {
-			shChildFolderItems.put(shFolderUtils.toJSON(shChildFolder));
-		}
-		return shChildFolderItems;
-	}
-
-	private ShFolder shFolderItemFactory(ShPost shPostItem) {
-		ShFolder shFolderItem = null;
-		if (shPostItem.getShPostType().getName().equals(ShSystemPostType.FOLDER_INDEX)) {
-			shFolderItem = shPostItem.getShFolder();
-		}
-
-		return shFolderItem;
-	}
-
-	private Map<String, ShPostAttr> shFolderPageLayoutMapFactory(ShPost shPostItem) {
-		Map<String, ShPostAttr> shFolderIndexMap = shPostUtils.postToMap(shPostItem);
-
-		ShPost shFolderPageLayout = shPostRepository
-				.findByTitle(shFolderIndexMap.get(ShSystemPostTypeAttr.PAGE_LAYOUT).getStrValue());
-
-		return shPostUtils.postToMap(shFolderPageLayout);
-	}
-
-	private String shPageLayoutFactory(String javascriptVar, String pageLayoutJS,
-			String pageLayoutHTML) throws ScriptException, IOException {
-	
-		StringBuilder shObjectJS = this.shObjectJSFactory();
-
-		String javascript = javascriptVar + pageLayoutJS;
-
-		// Nashorn Engine
-		ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-		Bindings bindings = engine.createBindings();
-		bindings.put("html", pageLayoutHTML);
-		bindings.put("spring", context);
-		Object pageLayoutResult = engine.eval(javascript, bindings);
-
-		Document doc = Jsoup.parse(pageLayoutResult.toString());
-
-		Elements elements = doc.getElementsByAttribute("sh-region");
-
-		// Regions
-		for (Element element : elements) {
-
-			String regionAttr = element.attr("sh-region");
-
-			ShPost shRegionPost = shPostRepository.findByTitle(regionAttr);
-
-			Map<String, ShPostAttr> shRegionPostMap = shPostUtils.postToMap(shRegionPost);
-
-			String shRegionJS = shRegionPostMap.get(ShSystemPostTypeAttr.JAVASCRIPT).getStrValue();
-
-			String shRegionHTML = shRegionPostMap.get(ShSystemPostTypeAttr.HTML).getStrValue();
-
-			bindings.put("html", shRegionHTML);
-
-			javascript = shObjectJS.toString() + javascriptVar + shRegionJS;
-
-			Object regionResult = engine.eval(javascript, bindings);
-
-			element.html(regionResult.toString());
-		}
-		return doc.html();
-	}
 
 	private String siteContext(String shSiteName, String shFormat, String shLocale, int contextPathPosition,
 			HttpServletRequest request, HttpServletResponse response) throws IOException, ScriptException {
 
-		ArrayList<String> contentPath = this.contentPathFactory(contextPathPosition, request);
+		ArrayList<String> contentPath = shSitesContextComponent.contentPathFactory(contextPathPosition, request);
 
 		ShSite shSite = shSiteRepository.findByFurl(shSiteName);
 
-		String objectName = this.objectNameFactory(contentPath);
+		String objectName = shSitesContextComponent.objectNameFactory(contentPath);
 
-		String folderPath = this.folderPathFactory(contentPath);
+		String folderPath = shSitesContextComponent.folderPathFactory(contentPath);
 
-		ShPost shPostItem = this.shPostItemFactory(shSite, folderPath, objectName);
+		ShPost shPostItem = shSitesContextComponent.shPostItemFactory(shSite, folderPath, objectName);
 
 		String javascriptVar = null;
 
@@ -342,14 +135,14 @@ public class ShSitesContext {
 		// Folder
 		if (shPostItem != null && shPostItem.getShPostType().getName().equals(ShSystemPostType.FOLDER_INDEX)) {
 
-			ShFolder shFolderItem = this.shFolderItemFactory(shPostItem);
+			ShFolder shFolderItem = shSitesContextComponent.shFolderItemFactory(shPostItem);
 
-			Map<String, ShPostAttr> shFolderPageLayoutMap = this.shFolderPageLayoutMapFactory(shPostItem);
+			Map<String, ShPostAttr> shFolderPageLayoutMap = shSitesContextComponent.shFolderPageLayoutMapFactory(shPostItem);
 
 			pageLayoutHTML = shFolderPageLayoutMap.get(ShSystemPostTypeAttr.HTML).getStrValue();
 			pageLayoutJS = shFolderPageLayoutMap.get(ShSystemPostTypeAttr.JAVASCRIPT).getStrValue();
 
-			JSONObject shThemeAttrs = this
+			JSONObject shThemeAttrs = shSitesContextComponent
 					.shThemeFactory(shFolderPageLayoutMap.get(ShSystemPostTypeAttr.THEME).getStrValue());
 
 			JSONObject shPostItemAttrs = shPostUtils.toJSON(shPostItem);
@@ -358,8 +151,8 @@ public class ShSitesContext {
 			JSONObject shFolderItemAttrs = shFolderUtils.toJSON(shFolderItem);
 
 			shFolderItemAttrs.put("theme", shThemeAttrs);
-			shFolderItemAttrs.put("posts", this.shPostItemsFactory(shFolderItem));
-			shFolderItemAttrs.put("folders", this.shChildFolderItemsFactory(shFolderItem));
+			shFolderItemAttrs.put("posts", shSitesContextComponent.shPostItemsFactory(shFolderItem));
+			shFolderItemAttrs.put("folders", shSitesContextComponent.shChildFolderItemsFactory(shFolderItem));
 			shFolderItemAttrs.put("post", shPostItemAttrs);
 			shFolderItemAttrs.put("site", shSiteUtils.toJSON(shSite));
 
@@ -376,7 +169,7 @@ public class ShSitesContext {
 			pageLayoutHTML = shPostPageLayoutMap.get(ShSystemPostTypeAttr.HTML).getStrValue();
 			pageLayoutJS = shPostPageLayoutMap.get(ShSystemPostTypeAttr.JAVASCRIPT).getStrValue();
 
-			JSONObject shThemeAttrs = this
+			JSONObject shThemeAttrs = shSitesContextComponent
 					.shThemeFactory(shPostPageLayoutMap.get(ShSystemPostTypeAttr.THEME).getStrValue());
 
 			JSONObject shSiteItemAttrs = shSiteUtils.toJSON(shSite);
@@ -390,7 +183,7 @@ public class ShSitesContext {
 		
 		}
 			
-		String shPageLayoutHTML = this.shPageLayoutFactory(javascriptVar, pageLayoutJS, pageLayoutHTML);
+		String shPageLayoutHTML = shSitesContextComponent.shPageLayoutFactory(javascriptVar, pageLayoutJS, pageLayoutHTML);
 
 		response.setContentType(MediaType.TEXT_HTML_VALUE);
 
