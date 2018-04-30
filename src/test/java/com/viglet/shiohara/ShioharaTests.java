7b57e331b8ea0fd895e6d2a985068c85b831a7c0
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-04-29 23:06:34 -0300

    - Gradle 4.7

--- src/test/java/com/viglet/shiohara/ShioharaTests.java
+++ src/test/java/com/viglet/shiohara/ShioharaTests.java
@@ -1,14 +1,38 @@
 package com.viglet.shiohara;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ShioharaTests {
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void testVersion() throws Exception {
+		mockMvc.perform(get("/api/v2")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$.product").value("Viglet Shiohara"));
+
 	}
 }
--- src/test/java/com/viglet/shiohara/api/widget/ShWidgetAPITest.java
+++ src/test/java/com/viglet/shiohara/api/widget/ShWidgetAPITest.java
@@ -6,14 +6,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.viglet.shiohara.ShioharaTests;
 
-public class ShWidgetAPITest extends ShioharaTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShWidgetAPITest {
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
