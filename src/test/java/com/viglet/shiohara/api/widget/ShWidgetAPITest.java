777a6b93d70aec9a535b1afbf51cc2499c5626fd
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-04-29 22:28:12 -0300

    - Spring Boot 2.1.0 and Unit Tests

---
+++ src/test/java/com/viglet/shiohara/api/widget/ShWidgetAPITest.java
@@ -0,0 +1,35 @@
+package com.viglet.shiohara.api.widget;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.viglet.shiohara.ShioharaTests;
+
+public class ShWidgetAPITest extends ShioharaTests {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void testAPIWidget() throws Exception {
+		mockMvc.perform(get("/api/v2/widget")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"));
+
+	}
+
+}
