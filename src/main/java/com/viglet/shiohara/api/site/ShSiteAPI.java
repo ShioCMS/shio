db9a1a9a2a4456951e54673f94256ed33f4a7a5c
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-05-24 11:52:34 -0300

    - Nested Relator

--- src/main/java/com/viglet/shiohara/api/site/ShSiteAPI.java
+++ src/main/java/com/viglet/shiohara/api/site/ShSiteAPI.java
@@ -363,40 +363,7 @@ public class ShSiteAPI {
 				shPostExchange.setGlobalId(shPost.getShGlobalId().getId());
 				Map<String, Object> fields = new HashMap<String, Object>();
 
-				for (ShPostAttr shPostAttr : shPostAttrRepository.findByShPost(shPost)) {
-					if (shPostAttr != null && shPostAttr.getShPostTypeAttr() != null) {
-						if (shPostAttr.getShPostTypeAttr().getShWidget().getName().equals(ShSystemWidget.RELATOR)) {
-							ShRelatorExchange shRelatorExchange = new ShRelatorExchange();
-							shRelatorExchange.setId(shPostAttr.getId());
-							shRelatorExchange.setName(shPostAttr.getStrValue());
-							Set<Object> relators = new HashSet<Object>();
-							for (ShRelatorItem shRelatorItem : shPostAttr.getShChildrenRelatorItems()) {
-								Map<String, Object> relatorFields = new HashMap<String, Object>();
-								for (ShPostAttr shPostAttrRelator : shRelatorItem.getShChildrenPostAttrs()) {
-									relatorFields.put(shPostAttrRelator.getShPostTypeAttr().getName(),
-											shPostAttrRelator.getStrValue());
-								}
-								relators.add(relatorFields);
-							}
-							shRelatorExchange.setShSubPosts(relators);
-							fields.put(shPostAttr.getShPostTypeAttr().getName(), shRelatorExchange);
-						} else {
-							fields.put(shPostAttr.getShPostTypeAttr().getName(), shPostAttr.getStrValue());
-						}
-
-						if (shPostAttr.getShPostTypeAttr().getName().equals(ShSystemPostTypeAttr.FILE)
-								&& shPost.getShPostType().getName().equals(ShSystemPostType.FILE)) {
-							String fileName = shPostAttr.getStrValue();
-							File directoryPath = shStaticFileUtils.dirPath(shPost.getShFolder());
-							File file = new File(directoryPath.getAbsolutePath().concat(File.separator + fileName));
-							ShFileExchange shFileExchange = new ShFileExchange();
-							shFileExchange.setGlobalId(shPost.getShGlobalId().getId());
-							shFileExchange.setId(shPost.getId());
-							shFileExchange.setFile(file);
-							files.add(shFileExchange);
-						}
-					}
-				}
+				this.shPostAttrExchangeIterate(shPost, shPostAttrRepository.findByShPost(shPost), fields, files);
 
 				shPostExchange.setFields(fields);
 
@@ -425,4 +392,39 @@ public class ShSiteAPI {
 		return shExchange;
 	}
 
+	public void shPostAttrExchangeIterate(ShPost shPost, Set<ShPostAttr> shPostAttrs, Map<String, Object> fields,
+			List<ShFileExchange> files) {
+		for (ShPostAttr shPostAttr : shPostAttrs) {
+			if (shPostAttr != null && shPostAttr.getShPostTypeAttr() != null) {
+				if (shPostAttr.getShPostTypeAttr().getShWidget().getName().equals(ShSystemWidget.RELATOR)) {
+					ShRelatorExchange shRelatorExchange = new ShRelatorExchange();
+					shRelatorExchange.setId(shPostAttr.getId());
+					shRelatorExchange.setName(shPostAttr.getStrValue());
+					Set<Object> relators = new HashSet<Object>();
+					for (ShRelatorItem shRelatorItem : shPostAttr.getShChildrenRelatorItems()) {
+						Map<String, Object> relatorFields = new HashMap<String, Object>();
+						this.shPostAttrExchangeIterate(shPost, shRelatorItem.getShChildrenPostAttrs(), relatorFields,
+								files);
+						relators.add(relatorFields);
+					}
+					shRelatorExchange.setShSubPosts(relators);
+					fields.put(shPostAttr.getShPostTypeAttr().getName(), shRelatorExchange);
+				} else {
+					fields.put(shPostAttr.getShPostTypeAttr().getName(), shPostAttr.getStrValue());
+				}
+
+				if (shPostAttr.getShPostTypeAttr().getName().equals(ShSystemPostTypeAttr.FILE)
+						&& shPost.getShPostType().getName().equals(ShSystemPostType.FILE)) {
+					String fileName = shPostAttr.getStrValue();
+					File directoryPath = shStaticFileUtils.dirPath(shPost.getShFolder());
+					File file = new File(directoryPath.getAbsolutePath().concat(File.separator + fileName));
+					ShFileExchange shFileExchange = new ShFileExchange();
+					shFileExchange.setGlobalId(shPost.getShGlobalId().getId());
+					shFileExchange.setId(shPost.getId());
+					shFileExchange.setFile(file);
+					files.add(shFileExchange);
+				}
+			}
+		}
+	}
 }
