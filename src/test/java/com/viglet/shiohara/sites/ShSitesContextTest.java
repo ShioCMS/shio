4b4e4cecc945438f2c0d9e424d42df908eee872a
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-05-26 11:03:05 -0300

    - Unit Tests: SitesContext

---
+++ src/test/java/com/viglet/shiohara/sites/ShSitesContextTest.java
@@ -0,0 +1,38 @@
+package com.viglet.shiohara.sites;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShSitesContextTest {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void sitesFullGeneric() throws Exception {
+		mockMvc.perform(get("/sites/viglet/default/en-us.")).andExpect(status().isOk());
+
+	}
+
+}
