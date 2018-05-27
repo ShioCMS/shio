336c1b6d50ca0c67945656a4f020f5cea0468d52
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-05-26 23:11:21 -0300

    - Post Type Editor UI Improvements

--- src/main/resources/js/client-side/src/main/__shFactory.js
+++ src/main/resources/js/client-side/src/main/__shFactory.js
@@ -1,4 +1,4 @@
 var shioharaApp = angular.module('shioharaApp', [ 'ngCookies', 'ngResource',
 		'ngAnimate', 'ngSanitize', 'ui.router', 'ui.bootstrap',
 		'pascalprecht.translate', 'angularMoment', 'ui.ace', 'ngFileUpload',
-		'ui-notification', 'ui.tinymce' ]);
+		'ui-notification', 'ui.tinymce', 'ui.sortable' ]);
