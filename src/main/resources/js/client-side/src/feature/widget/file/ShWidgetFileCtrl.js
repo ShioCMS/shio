3033de9f5863967cfbd43eef29e112d33805e297
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-06-17 11:46:30 -0300

    - File Widget with Image preview

--- src/main/resources/js/client-side/src/feature/widget/file/ShWidgetFileCtrl.js
+++ src/main/resources/js/client-side/src/feature/widget/file/ShWidgetFileCtrl.js
@@ -4,7 +4,8 @@ shioharaApp.controller('ShWidgetFileCtrl', [
 		'$timeout',
 		'$uibModal',
 		'shWidgetFileFactory',
-		function($scope, Upload, $timeout, $uibModal, shWidgetFileFactory) {
+		'shAPIServerService',
+		function($scope, Upload, $timeout, $uibModal, shWidgetFileFactory, shAPIServerService) {
 			$scope.fileName = null;
 			$scope.uploadNewFile = false;
 			$scope.$watch('shPostAttr.file', function() {
@@ -14,6 +15,40 @@ shioharaApp.controller('ShWidgetFileCtrl', [
 					$scope.shPostAttr.strValue = $scope.fileName;
 				}
 			});
+			$scope.imageURL = null;
+			$scope.breadcrumbFile = [];
+			$scope.init = function(shPost, shPostAttr) {
+				if (typeof shPostAttr.referenceObjects[0] != 'undefined' ) {
+					$scope.folderBreadcrumb($scope.breadcrumbFile,
+						shPostAttr.referenceObjects[0].shFolder);
+					$scope.getImageURL(shPostAttr.referenceObjects[0]);
+					if (shPostAttr.referenceObjects[0].title.match(/.(jpg|jpeg|png|gif)$/i)) {
+						$scope.imageURL = shAPIServerService.get().concat("/v2/object/" + shPostAttr.referenceObjects[0].id + "/preview");
+					}
+				}
+				else {
+					$scope.getImageURL(shPost);
+				}
+				
+			}
+
+			$scope.folderBreadcrumb = function(breadcrumb, shFolder) {
+				if (shFolder != null) {
+					var breadcrumbItem = {};
+					breadcrumbItem.id = shFolder.id;
+					breadcrumbItem.name = shFolder.name;
+					breadcrumb.push(breadcrumbItem);
+					if (shFolder.parentFolder != null) {
+						$scope.folderBreadcrumb(breadcrumb,
+								shFolder.parentFolder);
+					} else if (shFolder.shSite != null) {
+						var breadcrumbItem = {};
+						breadcrumbItem.id = shFolder.shSite.id;
+						breadcrumbItem.name = shFolder.shSite.name + " (Site)";
+						breadcrumb.push(breadcrumbItem);
+					}
+				}
+			}
 
 			$scope.newFile = function() {
 				$scope.uploadNewFile = true;
@@ -32,8 +67,31 @@ shioharaApp.controller('ShWidgetFileCtrl', [
 					$scope.fileName = shPostSelected.title;
 					shPostAttr.strValue = shPostSelected.id;
 					shPostAttr.referenceObjects = [ shPostSelected ];
+					$scope.getImageURL(shPostAttr.referenceObjects[0]);
 				}, function() {
 					// Selected CANCEL
 				});
 			}
+
+			$scope.selectFileByObject = function(shPostAttr, shObject) {
+				var modalInstance = shWidgetFileFactory
+						.modalSelectFile(shObject);
+				modalInstance.result.then(function(shPostSelected) {
+					// Selected INSERT
+					$scope.uploadNewFile = false;
+					$scope.fileName = shPostSelected.title;
+					shPostAttr.strValue = shPostSelected.id;
+					shPostAttr.referenceObjects = [ shPostSelected ];
+					$scope.getImageURL(shPostAttr.referenceObjects[0]);
+				}, function() {
+					// Selected CANCEL
+				});
+			}
+			
+			$scope.getImageURL = function(shPost) {
+				if (shPost.title.match(/.(jpg|jpeg|png|gif)$/i)) {
+					$scope.imageURL = shAPIServerService.get().concat("/v2/object/" + shPost.id + "/preview");
+				}
+			}
+
 		} ]);
