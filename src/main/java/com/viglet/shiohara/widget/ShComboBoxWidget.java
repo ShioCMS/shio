7bd20cd31004dbce82013e832bc8aba1d2fe1525
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2018-07-20 00:07:05 -0300

    - Site Form: Combo Box Widget

--- src/main/java/com/viglet/shiohara/widget/ShComboBoxWidget.java
+++ src/main/java/com/viglet/shiohara/widget/ShComboBoxWidget.java
@@ -1,5 +1,6 @@
 package com.viglet.shiohara.widget;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import com.viglet.shiohara.persistence.model.post.type.ShPostTypeAttr;
@@ -9,10 +10,22 @@ public class ShComboBoxWidget implements ShWidgetImplementation {
 
 	public String render(ShPostTypeAttr shPostTypeAttr) {
 		StringBuilder sb = new StringBuilder();
+		String widgetSettings = shPostTypeAttr.getWidgetSettings();
+		JSONObject settings = new JSONObject(widgetSettings);
+		String[] choices = settings.getString("choices").split("\n");
 		sb.append("<div class=\"form-group\">");
 		sb.append("<label>" + shPostTypeAttr.getLabel() + "</label>");
-		sb.append("<input type=\"text\" class=\"form-control\" size=\"50\" name=\"__sh-post-type-attr-"
-				+ shPostTypeAttr.getName() + "\" />");
+		sb.append("\"<select class=\"form-control\" name=\"__sh-post-type-attr-" + shPostTypeAttr.getName() + "\">");
+		for (String choice : choices) {
+			String[] choiceKV = choice.split(":");
+			String name = choiceKV[0].trim();
+			String label = choiceKV[1].trim();
+
+			sb.append("<option value=\"" + name + "\">" + label + "</option>");
+		}
+
+		sb.append("</select>");
+
 		sb.append("</div>");
 		return sb.toString();
 	}
