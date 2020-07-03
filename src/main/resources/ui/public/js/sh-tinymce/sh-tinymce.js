fb4a57d1eaef783e292d8688483f5e5634d5c33a
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-07-03 13:05:50 -0300

    - sh-tinymce updated

--- src/main/resources/ui/public/js/sh-tinymce/sh-tinymce.js
+++ src/main/resources/ui/public/js/sh-tinymce/sh-tinymce.js
@@ -1,33 +1,47 @@
 tinymce.PluginManager.add('shTinyMCE', function (editor, url) {
 
-    editor.addButton('shMailTo', {
-        title: 'Insert e-mail link',
+    editor.ui.registry.addButton('shMailTo', {
+        tooltip: 'Insert e-mail link',
         //cmd: 'mceMailTo',
-        image: '/img/widget/tinymce/email-icon.png',
-        onclick: function() {
+        icon: 'insert-time',
+        onAction: function () {
             // Open window
             editor.windowManager.open({
-              title: 'Insert e-mail link',              
-              body: [
-                {type: 'textbox', size: 40, name: 'email', label: 'e-mail'},
-                {type: 'textbox', size: 40, name: 'textDisplay', label: 'Text to display', value : editor.selection.getContent({ format: 'text' })}
-              ],
-              onsubmit: function(e) {
-               
-                var newText = "<a href='mailto:" + e.data.email + "'>" + e.data.textDisplay + "</a>"
-                editor.execCommand('mceInsertContent', false, newText);
-              }
+                title: 'Insert e-mail link',
+                body: {
+                    type: 'panel',
+                    items: [{ type: 'input', size: 40, name: 'email', label: 'e-mail' },
+                    { type: 'input', size: 40, name: 'textDisplay', label: 'Text to display', value: editor.selection.getContent({ format: 'text' }) }] // array of panel components
+                },
+                buttons: [ // A list of footer buttons
+                    {
+                        type: 'cancel',
+                        name: 'closeButton',
+                        text: 'Cancel'
+                    },
+                    {
+                        type: 'submit',
+                        name: 'submitButton',
+                        text: 'Insert Email',
+                        primary: true
+                    }
+                ],
+                onSubmit: function (api) {
+                    var data = api.getData();
+                    var newText = "<a href='mailto:" + data.email + "'>" + data.textDisplay + "</a>"
+                    tinymce.activeEditor.execCommand('mceInsertContent', false, newText);
+                    api.close();
+                }
             });
-          }
+        }
 
     });
 
     // Add a button that opens a window
-    editor.addButton('shAddImage', {
+    editor.ui.registry.addButton('shAddImage', {
         tooltip: 'Insert Shio image',
-        icon: true,
-        image: '/img/widget/tinymce/image-icon.png',
-        onclick: function () {
+        icon: 'insert-time',
+        onAction: function () {
 
             var scope = angular.element(document.getElementById('idForJS')).scope();
             scope.modalExplorer().then(function (rest) {
@@ -38,11 +52,10 @@ tinymce.PluginManager.add('shTinyMCE', function (editor, url) {
             });
         }
     });
-    editor.addButton('shAddContent', {
+    editor.ui.registry.addButton('shAddContent', {
         tooltip: 'Insert Shio content',
-        icon: true,
-        image: '/img/widget/tinymce/content-icon.png',
-        onclick: function () {
+        icon: 'insert-time',
+        onAction: function () {
             var linkText = editor.selection.getContent({ format: 'text' });
             var scope = angular.element(document.getElementById('idForJS')).scope();
             scope.selectContent().then(function (rest) {
@@ -57,10 +70,10 @@ tinymce.PluginManager.add('shTinyMCE', function (editor, url) {
         }
     });
     // Adds a menu item to the tools menu
-    editor.addMenuItem('shAddImage', {
+    editor.ui.registry.addMenuItem('shAddImage', {
         text: 'Example plugin',
         context: 'tools',
-        onclick: function () {
+        onAction: function () {
             // Open window with a specific url
             editor.windowManager.open({
                 title: 'TinyMCE site',
